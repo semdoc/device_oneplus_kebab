e42c4cc406dc5bb54cb51a5c96dd644bf8bd2c3f
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-03-01 13:22:06 +0800

    kebab: Set default hotspot ssid by RRO
    Change-Id: Ia270eedade01277e41c2c2fc47b72e4ffbe823c3

--- device.mk
+++ device.mk
@@ -50,6 +50,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/odm_feature_list:$(TARGET_COPY_OUT_ODM)/etc/odm_feature_list
 
+# Wifi Overlay
+PRODUCT_PACKAGES += \
+    OnePlus8TWifiOverlay
+
+# API Level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 # DeviceParts
